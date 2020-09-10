ee64020ecd19257697229e9e78a2e0f16e8dbbef
Fabian Friedl <friedlfabian@gmx.de>
2020-09-10 19:53:53 +0200

    Added project base data

---
+++ dotnet/ResourcesAPI/ResourcesAPI/API.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ResourcesAPI
+{
+    public class API
+    {
+        public string Key { get; private set; }
+
+        public API(string key)
+        {
+            this.Key = key;
+        }
+    }
+}
