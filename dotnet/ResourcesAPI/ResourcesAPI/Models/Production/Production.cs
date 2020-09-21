5ad3cbaf79dcc1f3bafb1377313b4a67e6f04ce0
Fabian Friedl <friedlfabian@gmx.de>
2020-09-21 19:03:25 +0200

    Implemented production models for data loading

---
+++ dotnet/ResourcesAPI/ResourcesAPI/Models/Production/Production.cs
@@ -0,0 +1,33 @@
+﻿namespace ResourcesAPI.Models.Production
+{
+    public class Production
+    {
+        public ushort ItemID { get; private set; }
+
+        public string ItemName { get; private set; }
+
+        public ushort FactoryID { get; private set; }
+
+        public string FactoryName { get; private set; }
+
+        public int BaseOutputPerHour { get; private set; }
+
+        public int OutputPerCycle { get; private set; }
+
+        public int CreditsPerCycle { get; private set; }
+
+        public ProductionIngredient[] Ingredients { get; private set; }
+
+        public Production(ushort itemId, string itemName, ushort factoryId, string factoryName, int baseOutputPerHour, int outputPerCylce, int creditsPerCycle, params ProductionIngredient[] ingredients)
+        {
+            this.ItemID = itemId;
+            this.ItemName = itemName;
+            this.FactoryID = factoryId;
+            this.FactoryName = factoryName;
+            this.BaseOutputPerHour = baseOutputPerHour;
+            this.OutputPerCycle = outputPerCylce;
+            this.CreditsPerCycle = creditsPerCycle;
+            this.Ingredients = ingredients;
+        }
+    }
+}
---
+++ dotnet/ResourcesAPI/ResourcesAPI/Models/Production/ProductionIngredient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResourcesAPI.Models.Production
+{
+    public class ProductionIngredient
+    {
+        public ushort ItemID { get; private set; }
+
+        public int QuantityPerCycle { get; private set; }
+
+        public ProductionIngredient(ushort id, int quantityPerCylce)
+        {
+            this.ItemID = id;
+            this.QuantityPerCycle = quantityPerCylce;
+        }
+    }
+}
